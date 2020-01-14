96f7a4ca595cccc29dfe651e774dad3e2a189ff7
G3Dav <56224244+G3Dav@users.noreply.github.com>
2020-01-14 12:19:58 -0500

    Update bmr.js

--- bmr.js
+++ bmr.js
@@ -11,6 +11,6 @@ function calc() {
   bmr = Math.trunc(66+( 6.23 * weight ) + ( 12.7 * height ) - ( 6.8 * age ));
   }
   else {
-  bmr = Math.trunc(655 + ( 6.23 * weight) + ( 12.7 * height) - (6.8 * age ));
+  bmr = Math.trunc(655 + ( 4.35 * weight) + ( 4.7 * height) - (4.7 * age ));
   }
 }
